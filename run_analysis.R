5409dce062545fabeb9147c87177db0c17483227
crodger1 <crodger1@gmail.com>
2015-02-22 10:42:22 -0500

    updated version with summary file created

--- run_analysis.R
+++ run_analysis.R
@@ -1,35 +1,33 @@
-# You should create one R script called run_analysis.R that does the following. 
+# this does the following:
 # Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Extracts the measurements on the mean and standard deviation for each measurement.
 # Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names. 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-# pseudo code:
-# read subject_test
-# merge to the right with y_test (cbind)
-# merge to the right with x_test (cbind)
-# this is one dataset
+# labels the data set with descriptive variable names.
 
 
 # repeat process with train directory (cbind subject train with y_train and x_train)
+
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
 
 setwd("C:/Users/crodger/Documents/GitHub/Getting-and-Cleaning-Data-Course-Project")
 
 ## read in the labels for the data and convert to a vector to be used as names
   features<-read.table("./UCI HAR Dataset/features.txt",stringsAsFactors=FALSE)
   vnames<-features[,2]
-  ## create an index for the columns that should be kept
+  ## create an index for the columns that should be kept by matching
+  ## to text "mean()" and "std()" (non-case sensitive)
   means<-grep("mean()",vnames,ignore.case=TRUE)
   stds<-grep("std()",vnames,ignore.case=TRUE)
-  keep<-sort(c(means,stds))
+  keep<-sort(c(means,stds))  # used to retain original var sequence
 
   ## read in the main analysis datasets (x_test and x_train)
   x_test<-read.table("./UCI HAR Dataset/test/x_test.txt")
   head(x_test[,3:6])
-  attr(x_test, "names")<-vnames
+  attr(x_test, "names")<-vnames # adds variable names based on
   head(x_test[,3:6])
-    ## restrict to means and standard deviations
+    ## restrict to means and standard deviations data only
     x_test<-x_test[,keep]
     head(x_test[,1:6])
 
@@ -64,7 +62,7 @@ setwd("C:/Users/crodger/Documents/GitHub/Getting-and-Cleaning-Data-Course-Projec
   subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
   attr(subject_train, "names")<-c("Subject")
   head(subject_train)
-  
+
   y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
   attr(y_train, "names")<-c("Activity")
   head(y_train)
@@ -80,11 +78,15 @@ setwd("C:/Users/crodger/Documents/GitHub/Getting-and-Cleaning-Data-Course-Projec
 
   ## create a list of variables to sumamrize (need to exclude the grouping vars of Subject and Activity)
   vars<-attr(fulldata[,-c(1,2)],"names")
-  
+
   library(dplyr)
-  
+
   mean_data <- fulldata %>%
     group_by(Subject,Activity) %>%
     summarise_each(funs(mean))
 
-  
+
+
+write.table(mean_data,"./summary_file.txt",row.name=FALSE)
+
+
