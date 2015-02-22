029d92c55dee015a1910ca186c07b510117a9f24
Chris Rodger <crodger1@gmail.com>
2015-02-22 15:47:45 -0500

    final updates to code, data dictionary, and readme

--- README.md
+++ README.md
@@ -2,17 +2,23 @@
 
 This folder contains the code and data for the project assignment for the Coursera "Getting and Cleaning Data" course (Coursera getdata-011)
 
-The instructions for the assingment were as follows:
+For this assignment, we created a run_analysis.R file that takes Samsung activity monitoring data and manipulates it to generate mean values for the collected mean and standard deviation data, and summarizes them by subject (individual for whom the data were collected) and activity type (e.g. Walking).
 
-* You should create one R script called run_analysis.R that does the following. 
-* Merges the training and the test sets to create one data set.
-* Extracts only the measurements on the mean and standard deviation for each measurement. 
-* Uses descriptive activity names to name the activities in the data set
-* Appropriately labels the data set with descriptive variable names. 
-* From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+This script starts with the assumption that the Samsung data is available in the working directory in an unzipped UCI HAR Dataset folder.  The data source is noted below
 
-The data were downloaded from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Specifically, run_analysis.R does the following:
+
+* imports the source data downloaded from the UCI
+* adds variable names to the columns in the test and train data
+* restricts variables to the set containing the specific test "mean()", "Mean()", "std()", or "Std()"
+* combines the source datasets into a single data set
+* uses value labels to add descriptive activity names to the 6 activity types
+* creates a tidy dataset with the _average_ of each mean and standard deviation (std) in the source data
+* this final dataset is collapsed to the Source, Subject, and Activity level (one record for each activity type per subject)
+
+A data dictionary is provided (data_dictionary.txt) which describes the included variables
+
+The source data  were downloaded from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 There is more information about this dataset here: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
-
-
--- run_analysis.R
+++ run_analysis.R
@@ -1,92 +1,89 @@
-# this does the following:
-# Merges the training and the test sets to create one data set.
-# Extracts the measurements on the mean and standard deviation for each measurement.
-# Uses descriptive activity names to name the activities in the data set
-# labels the data set with descriptive variable names.
+# please see the provided README.md file for an overview of this code
 
 
-# repeat process with train directory (cbind subject train with y_train and x_train)
-
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-
-
-setwd("C:/Users/crodger/Documents/GitHub/Getting-and-Cleaning-Data-Course-Project")
-
-## read in the labels for the data and convert to a vector to be used as names
+## read in the variable names for the data and convert to a vector to be used as names
   features<-read.table("./UCI HAR Dataset/features.txt",stringsAsFactors=FALSE)
   vnames<-features[,2]
   ## create an index for the columns that should be kept by matching
   ## to text "mean()" and "std()" (non-case sensitive)
-  means<-grep("mean()",vnames,ignore.case=TRUE)
-  stds<-grep("std()",vnames,ignore.case=TRUE)
-  keep<-sort(c(means,stds))  # used to retain original var sequence
-
-  ## read in the main analysis datasets (x_test and x_train)
-  x_test<-read.table("./UCI HAR Dataset/test/x_test.txt")
-  head(x_test[,3:6])
-  attr(x_test, "names")<-vnames # adds variable names based on
-  head(x_test[,3:6])
-    ## restrict to means and standard deviations data only
-    x_test<-x_test[,keep]
-    head(x_test[,1:6])
-
-  x_train<-read.table("./UCI HAR Dataset/train/x_train.txt")
-  head(x_train[,3:6])
-  attr(x_train, "names")<-vnames
-  head(x_train[,3:6])
-    ## restrict to means and standard deviations
-    x_train<-x_train[,keep]
-    head(x_train[,1:6])
+  means<-grep("mean()",vnames,fixed=TRUE)  # keeps if text contains "mean()"
+  Means<-grep("Mean()",vnames,fixed=TRUE)  # keeps if text contains "mean()"
+  stds<-grep("std()",vnames,fixed=TRUE)    # keeps if text contains "std()"
+  Stds<-grep("Std()",vnames,fixed=TRUE)    # keeps if text contains "std()"
+  keep<-sort(c(means,Means,stds,Stds))     # used to retain original var sequence
 
 
+## read in the main analysis datasets (x_test and x_train)
+  ## read test data
+    x_test<-read.table("./UCI HAR Dataset/test/x_test.txt")
+    head(x_test[,4:10])             # checking table import
+    attr(x_test, "names")<-vnames   # adds variable names
+    head(x_test[,4:10])             # checking variable names added
+  ## restrict to means and standard deviations data only
+    x_test<-x_test[,keep]           # restricting to columns of interest
+    head(x_test[,4:10])             # confirming data restricted to cols of interest
 
-#grep("mean",vnames,ignore.case=TRUE)+2
-# x_test2<-x_test[,grep("mean",vnames,ignore.case=TRUE)]
-# head(x_test2[,1:10])
-
-  ## combine these data with the subject (person tested) and the activity (e.g. walking)
-# attr(x_test,"names")
-
-  subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
-  attr(subject_test, "names")<-c("Subject")
-  head(subject_test)
-
-  y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
-  attr(y_test, "names")<-c("Activity")
-  head(y_test)
-  ## combine the test data with subject and activity
-    test<-cbind(subject_test,y_test,x_test)
+  ## read train data
+    x_train<-read.table("./UCI HAR Dataset/train/x_train.txt")
+    head(x_train[,4:10])            # checking table import
+    attr(x_train, "names")<-vnames  # adds variable names
+    head(x_train[,4:10])            # checking variable names added
+  ## restrict to means and standard deviations
+    x_train<-x_train[,keep]         # restricting to columns of interest
+    head(x_train[,4:10])            # confirming data restricted to cols of interest
 
 
-  subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
-  attr(subject_train, "names")<-c("Subject")
-  head(subject_train)
+## add information on subject (person tested) and the activity (e.g. walking)
+  ## add this information to test data
+    subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+    attr(subject_test, "names")<-c("Subject")
+    head(subject_test)
 
-  y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
-  attr(y_train, "names")<-c("Activity")
-  head(y_train)
-  ## combine the train data with subject and activity
-    train<-cbind(subject_train,y_train,x_train)
+    y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
+    attr(y_test, "names")<-c("Activity")
+    head(y_test)
+    ## combine the test data with subject and activity, add variable indicating Source
+    test<-cbind(Source="Test",subject_test,y_test,x_test)
 
-  ## combine the full test and train data
+  ## add this information to the train data
+    subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+    attr(subject_train, "names")<-c("Subject")
+    head(subject_train)
+
+    y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
+    attr(y_train, "names")<-c("Activity")
+    head(y_train)
+
+    ## combine the train data with subject and activity, add variable indicating Source
+    train<-cbind(Source="Train",subject_train,y_train,x_train)
+
+
+## combine the full test and train data
+  ## start by checking what the dimensions should be
     dim(train)
     dim(test)
     nrow(train)+nrow(test)
+  ## combine and check new dimensions
     fulldata<-rbind(train,test)
     dim(fulldata)
 
-  ## create a list of variables to sumamrize (need to exclude the grouping vars of Subject and Activity)
-  vars<-attr(fulldata[,-c(1,2)],"names")
 
-  library(dplyr)
-
-  mean_data <- fulldata %>%
-    group_by(Subject,Activity) %>%
-    summarise_each(funs(mean))
+## change the Activity column to use activity name labels instead of just values
+    fulldata$Activity <- factor(fulldata$Activity,
+            levels = c(1,2,3,4,5,6),
+            labels = c("Walking", "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying") )
 
 
+## collapse the data to get the means for mean() and std() information
+  ## create a list of variables to sumamrize (exclude the grouping vars Source, Subject and Activity)
+    vars<-attr(fulldata[,-c(1,2,3)],"names")
 
-write.table(mean_data,"./summary_file.txt",row.name=FALSE)
+  ## use dplyr library to help with collapsing the data
+    library(dplyr)
 
+    mean_data <- fulldata %>%
+      group_by(Source,Subject,Activity) %>%
+      summarise_each(funs(mean))
 
+## export the data for uploading to course website
+    write.table(mean_data,"./summary_file.txt",row.name=FALSE)
